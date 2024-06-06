3f68f630d84aa712f280d4081dcd9db995c3dc8c
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-06-06 07:42:04 -0300

    add: i can send email with all expired  vacinnes

--- src/cronjobs/verificationValideVacine.ts
+++ src/cronjobs/verificationValideVacine.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-import cron from 'cron';
 import nodemailer from 'nodemailer';
+import cron from 'node-cron';
 
 const prisma = new PrismaClient();
 const transport = nodemailer.createTransport({
-  host: 'live.smtp.mailtrap.io',
+  service: 'gmail',
+  host: 'smtp.gmail.com',
   port: 587,
   auth: {
-    user: 'api',
-    pass: '6995b2ad572647d30b27c94a626ce0a5',
+    user: 'pedrohprado26@gmail.com',
+    pass: 'qzvt vnnd nbfm zltx',
   },
   tls: {
     rejectUnauthorized: false,
@@ -18,7 +19,7 @@ const transport = nodemailer.createTransport({
 const enviaEmail = async (mensage: string) => {
   const emailOptions = {
     from: 'mailtrap@demomailtrap.com',
-    to: 'pedro.pedertractor@gmail.com',
+    to: 'eric.souza@ptractor.com.br',
     subject: 'Vacinação Próxima',
     text: mensage,
   };
@@ -27,4 +28,51 @@ const enviaEmail = async (mensage: string) => {
   console.log('email teste');
 };
 
-enviaEmail('oi teste');
+const verificationVacines = async () => {
+  const oneWeekBefore = new Date();
+  oneWeekBefore.setDate(oneWeekBefore.getDate() + 7);
+
+  const vacines = await prisma.vacina.findMany({
+    where: {
+      proximaVacina: {
+        lte: oneWeekBefore,
+      },
+    },
+    include: {
+      animal: {
+        include: {
+          cliente: true,
+        },
+      },
+    },
+  });
+
+  if (vacines.length > 0) {
+    let emailBody = 'Vacinas prestes a vencer: \n \n';
+    vacines.forEach((vacina) => {
+      emailBody += `Cliente: ${vacina.animal.cliente.nome}, Animal: ${
+        vacina.animal.nome
+      }, Vacina: ${
+        vacina.nomeVacina
+      }, Data de Aplicação: ${vacina.dataAplicacao.toLocaleDateString(
+        'pt-br'
+      )}, numero do cliente: ${vacina.animal.cliente.telefone}\n`;
+    });
+
+    console.log(emailBody);
+
+    //call function for send email
+    // enviaEmail(emailBody);
+  }
+};
+
+cron.schedule(
+  '0 0 * * 5',
+  () => {
+    verificationVacines();
+  },
+  {
+    timezone: 'America/Sao_Paulo',
+  }
+);
+verificationVacines();
